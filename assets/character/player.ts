69d0f562d8933f274cb30d6a9c6229592a3e2742
Ruslan Enikeev <ruslan.enikeev@mytona.com>
2023-03-05 18:08:17 +0300

    123

--- assets/character/player.ts
+++ assets/character/player.ts
@@ -1,12 +1,10 @@
-
-import { _decorator, Component, Node, KeyCode, input, Input, systemEvent, SystemEventType, EventKeyboard, macro, Vec2, RigidBody2D, Collider2D, BoxCollider2D, Contact2DType, IPhysics2DContact, Label} from 'cc';
-import bridge from '@vkontakte/vk-bridge';
+import { _decorator, Component, Node, systemEvent, SystemEventType, EventKeyboard, macro, Vec2, RigidBody2D, Collider2D, BoxCollider2D, Contact2DType, IPhysics2DContact, Label } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Player')
 export class Player extends Component {
 
-    @property({type: Label})
+    @property({ type: Label })
     private scoreLabel: Label | null = null;
     private score: number = 0;
 
@@ -16,30 +14,29 @@ export class Player extends Component {
     private direction: number = 0;
     private walk_force: number = 1000;
     private jump_force: number = 35000;
-   
+
 
     onLoad() {
-        bridge.send("VKWebAppInit", {});
-        input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
-        input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
+        systemEvent.on(SystemEventType.KEY_DOWN, this.onKeyDown, this);
+        systemEvent.on(SystemEventType.KEY_UP, this.onKeyUp, this);
     }
 
-    start () {
+    start() {
         this.rigidbody = this.node.getComponent(RigidBody2D);
 
         this.collider = this.node.getComponent(BoxCollider2D);
-        if( this.collider ) {
+        if (this.collider) {
             this.collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
         }
     }
 
-    update (deltaTime: number) {
-        this.rigidbody.applyForceToCenter( new Vec2( this.direction * this.walk_force, 0 ), true );
+    update(deltaTime: number) {
+        this.rigidbody.applyForceToCenter(new Vec2(this.direction * this.walk_force, 0), true);
     }
 
-    onBeginContact( selfCollider: BoxCollider2D, otherCollider: BoxCollider2D, contact: IPhysics2DContact | null ) {
-        
-        if( otherCollider.name == "star<BoxCollider2D>" ){
+    onBeginContact(selfCollider: BoxCollider2D, otherCollider: BoxCollider2D, contact: IPhysics2DContact | null) {
+
+        if (otherCollider.name == "star<BoxCollider2D>") {
             otherCollider.node.getComponent("Star").destroyStar();
             this.score++;
             this.scoreLabel.string = "Score: " + this.score;
@@ -48,20 +45,20 @@ export class Player extends Component {
     }
 
     onKeyDown(event: EventKeyboard) {
-        switch ( event.keyCode ) {
-            case KeyCode.KEY_A:
-            case KeyCode.ARROW_LEFT: {
+        switch (event.keyCode) {
+            case macro.KEY.a:
+            case macro.KEY.left: {
                 this.direction = -1;
                 break;
             }
-            case KeyCode.KEY_D:
-            case KeyCode.ARROW_RIGHT: {
+            case macro.KEY.d:
+            case macro.KEY.right: {
                 this.direction = 1;
                 break;
             }
-            case KeyCode.SPACE:
-            case KeyCode.ARROW_UP: {
-                this.rigidbody.applyForceToCenter( new Vec2( 0, this.jump_force ), true );
+            case macro.KEY.space:
+            case macro.KEY.up: {
+                this.rigidbody.applyForceToCenter(new Vec2(0, this.jump_force), true);
                 break;
             }
             default:
@@ -70,14 +67,14 @@ export class Player extends Component {
     }
 
     onKeyUp(event: EventKeyboard) {
-        switch ( event.keyCode ) {
-            case KeyCode.KEY_A:
-            case KeyCode.ARROW_LEFT: {
+        switch (event.keyCode) {
+            case macro.KEY.a:
+            case macro.KEY.left: {
                 this.direction = 0;
                 break;
             }
-            case KeyCode.KEY_D:
-            case KeyCode.ARROW_RIGHT: {
+            case macro.KEY.d:
+            case macro.KEY.right: {
                 this.direction = 0;
                 break;
             }
